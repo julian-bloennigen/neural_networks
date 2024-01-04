1a244799c9c6353f83ade09ac732142d924ef711
Julian <118623124+julian-bloennigen@users.noreply.github.com>
2024-01-04 18:09:05 +0100

    g

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from numpy import loadtxt
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 
-dataset = loadtxt('aboutJulian/neural_networks/main.py', delimiter=",")
+dataset = loadtxt('data/diabetes.csv', delimiter=",")
 
 X = dataset[:,0:8]
 y= dataset[:,8]
